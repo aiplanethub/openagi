ad90d89b4c7bf1e5a1a9e03555c56bfe65fda9b2
ShreehariVaasishta <shreehari9481@gmail.com>
2024-05-06 17:26:30 +0530

    Get next unprocessed task
    * Modify logic to maintain a separate queue to maintain processed tasks

--- src/openagi/tasks/lists.py
+++ src/openagi/tasks/lists.py
@@ -7,6 +7,7 @@ from openagi.tasks.task import Task
 class TaskLists:
     def __init__(self) -> None:
         self.tasks = Queue()
+        self.completed_tasks = Queue()
 
     def add_task(self, task: Task) -> None:
         """Adds a Task instance to the queue."""
@@ -25,6 +26,10 @@ class TaskLists:
             return self.tasks.get_nowait()
         return None
 
-    def all_tasks_processed(self):
+    @property
+    def all_tasks_completed(self) -> bool:
         """Checks if all tasks in the queue have been processed."""
         return self.tasks.empty()
+
+    def add_completed_tasks(self, task: Task):
+        self.completed_tasks.put(task)
