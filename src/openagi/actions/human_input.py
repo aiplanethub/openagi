f7ccc49adbebdea77d870bfcd02c2082246de5c2
Bhavyaaiplanet <bhavya@aiplanet.com>
2024-05-15 12:44:18 +0530

    added human feedback logic

--- src/openagi/actions/human_input.py
+++ src/openagi/actions/human_input.py
@@ -7,5 +7,15 @@ class HumanCLIInput(BaseAction):
     query: str = Field(..., description="Query to ask the user")
 
     def execute(self):
-        response = input(self.query)
-        return response
+        ques_prompt = "Do you think this task is being executed as expected [y/n] or [yes/no]: "
+        feedback = ''
+        response = input(ques_prompt + self.query)
+
+        if response not in ['y' , 'n' , 'yes' , 'no' , 'NO' ,'YES' , 'No' , 'Yes']:
+            print("Please provide a valide answer something like : ('y' , 'n' , 'yes' , 'no' , 'NO' ,'YES' , 'No' , 'Yes')")
+            response , feedback = self.execute()
+
+        if response=='n':
+            feedback = input('Do provide your feedback on what you think is needed to be modified: ')
+
+        return response , feedback
