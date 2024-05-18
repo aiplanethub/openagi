3ffca1ca034bd1cbe3cd463c469faed7e21f63cf
ShreehariVaasishta <shreehari9481@gmail.com>
2024-05-18 14:09:18 +0530

    task execution fixes

--- src/openagi/actions/tools/ddg_search.py
+++ src/openagi/actions/tools/ddg_search.py
@@ -6,27 +6,36 @@ from duckduckgo_search import DDGS
 class DuckDuckGoSearch(BaseAction):
     """Search Tool to fetch results from  DuckDuckGo"""
 
+    name: str = Field(
+        default_factory=str,
+        description="DuckDuckGoSearch Action to search over duckduckgo using the query.",
+    )
+    description: str = Field(
+        default_factory=str,
+        description="Search for words, documents, images, videos, news, maps and text translation using the DuckDuckGo.com search engine.",
+    )
+
     query: str = Field(..., description="User query to fetch web search results from DuckDuckGo")
 
-    region: str = Field(
-        default="wt-wt",
-        description=" which part of the region data needs to clustered: wt-wt, us-en, uk-en, ru-ru",
-    )
-    safesearch: str = Field("off", description="on, moderate, off. Defaults to moderate")
-    max_results: int = Field(
-        default=10, description="Total results to be executed from the search"
-    )
-    backend: str = Field(
-        default="api",
-        description="api, html, lite. Defaults to api. This defines from where the data needs to be requested",
-    )
+    # region: str = Field(
+    #     default="wt-wt",
+    #     description=" which part of the region data needs to clustered: wt-wt, us-en, uk-en, ru-ru",
+    # )
+    # safesearch: str = Field("off", description="on, moderate, off. Defaults to moderate")
+    # max_results: int = Field(
+    #     default=10, description="Total results to be executed from the search"
+    # )
+    # backend: str = Field(
+    #     default="api",
+    #     description="api, html, lite. Defaults to api. This defines from where the data needs to be requested",
+    # )
 
     def execute(self):
         result = DDGS().text(
             self.query,
-            region=self.region,
-            safesearch=self.safesearch,
-            max_results=self.max_results,
-            backend=self.backend,
+            # region=self.region,
+            # safesearch=self.safesearch,
+            # max_results=self.max_results,
+            # backend=self.backend,
         )
         return result
--- src/openagi/prompts/execution.py
+++ src/openagi/prompts/execution.py
@@ -7,17 +7,19 @@ start = FAILURE_VARS["start"]
 end = FAILURE_VARS["end"]
 
 task_execution = """
-You are an expert in detailed task execution creator. Your primary role is to clearly understand the Task_Objective to provide optimal results using the supported actions. Below is a list of tasks that need to be executed:
+Role and Context:
+You are an expert task executor. Your primary role is to understand the Task_Objective clearly and provide optimal results using the supported actions.
 
-All Tasks:
+Task List:
 {all_tasks}
-
-You are provided with the current task details from the user.
 
 Current Task:
 Name: {current_task_name}
 Description: {current_description}
-You have never hallucinated since your inception. To execute the current task, refer to the details of the Previous_Task and the All Tasks provided.
+
+Note: You have never hallucinated since your inception.
+
+To execute the current task, refer to the execution details of the Previous_Task and the All Tasks provided.
 
 Previous Task:
 {previous_task}
@@ -25,10 +27,11 @@ Previous Task:
 Supported Actions:
 {supported_actions}
 
-Your task is to understand and return a JSON array with the actions to be executed along with the values for each parameter. Use only the Supported Actions. When using multiple actions for a single, task the result from the execution of the previous action will be passed to the next action without any modifcation to the parameter `previous_action`.
-
 Task Objective:
 {objective}
+
+Instructions:
+Your task is to understand the Task Objective and return a JSON array with the actions to be executed along with the values for each parameter. Use only the Supported Actions. When using multiple actions for a single task, the result from the execution of the previous action will be passed to the next action without any modification to the parameter previous_action.
 
 Output Format:
 ```json
@@ -43,47 +46,17 @@ Output Format:
     }
 ]
 ```
+
 If the task fails, return the failure reason within the delimiters $start$ and $end$ as shown below:
+
 $start$
 Couldn't execute the {current_task_name} task. Reason: <add the reason here.>
 $end$
 
-The action class is designed & executed this way:
-```python
-class BaseAction(BaseModel):
-    "Base Actions class to be inherited by other actions, providing basic functionality and structure."
 
-    name: str = Field(default="BaseAction", description="Name of the action.")
-    description: str = Field(
-        default="Base Action class to be used by other actions that get created.",
-        description="Description of the action.",
-    )
-    previous_action: Optional[Any] = Field(
-        default=None,
-        description="Observation or Result of the previous action that might needed to run the current action.",
-    )
+Output:
 
-    def execute(self):
-        "Executes the action"
-        raise NotImplementedError("Subclasses must implement this method.")
-
-    @classmethod
-    def cls_doc(cls):
-        return {
-            "cls": {
-                "kls": cls.__name__,
-                "module": cls.__module__,
-            },
-            "params": {
-                field_name: field.description for field_name, field in cls.model_fields.items()
-            },
-        }
-
-act = BaseAction(**params)
-res = act.execute()
-```
-
-Return the actions in JSON format as per the output format mentioned above, including the delimiters "json" "", without any other content in the response.
+Return the actions in JSON format as per the output format mentioned above, including the delimiters "json", without any other content in the response.
 """
 
 
