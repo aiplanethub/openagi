01afb842a61f4f87cb0e8d356713f2692f2f94b3
ShreehariVaasishta <shreehari9481@gmail.com>
2024-05-21 18:07:26 +0530

    rename a parameter

--- src/openagi/planner/task_decomposer.py
+++ src/openagi/planner/task_decomposer.py
@@ -16,7 +16,7 @@ class TaskPlanner(BasePlanner):
     human_intervene: bool = Field(
         default=True, description="If human internvention is required or not."
     )
-    actions: Optional[BaseAction] = Field(
+    input_action: Optional[BaseAction] = Field(
         default=HumanCLIInput,
         description="If `human_intervene` is enabled, which action to be performed.",
     )
@@ -47,7 +47,7 @@ class TaskPlanner(BasePlanner):
         prompt, ques_to_human = extract_ques_and_task(resp)
 
         while self.human_intervene and self._should_clarify(ques_to_human):
-            human_intervene = self.actions(ques_prompt=ques_to_human)
+            human_intervene = self.input_action(ques_prompt=ques_to_human)
             human_resp = human_intervene.execute()
             prompt = f"{prompt}\n{ques_to_human}\n{human_resp}"
             resp = self.llm.run(prompt)
