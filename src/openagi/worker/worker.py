48960a7104e64e6fbee2d62db50b1108206c4956
Tanish <76485783+tknishh@users.noreply.github.com>
2024-05-23 00:13:14 +0530

    worker - first commit

--- src/openagi/worker/worker.py
+++ src/openagi/worker/worker.py
@@ -1,18 +1,17 @@
-from typing import Any, Optional
+from typing import Optional
 from pydantic import BaseModel, Field
 from openagi.llms.base import LLMBaseModel
-from openagi.tools.base import BaseTool
-
+from openagi.actions.base import BaseAction
 
 class Worker(BaseModel):
-    role: str = Field(..., description="Role of the worker")
-    backstory: Optional[str] = Field(
-        default=None, description="Backstory for the worker"
-    )
-    tools: Optional[BaseTool] = Field(
+    description: str = Field(..., description="Capabilities of the Worker")
+    
+    actions: Optional[BaseAction] = Field(
         default_factory=[], description="Tools available for Worker"
     )
     llm: Optional[LLMBaseModel] = Field(
         description="LLM Model to be used.",
     )
-    st_memory: Optional[Any] = None
+
+    def __init__(self):
+        super().__init__()
