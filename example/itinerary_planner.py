49f3a132cdbfdf7fb3d5bedd6d678eb70647a7ae
ShreehariVaasishta <shreehari9481@gmail.com>
2024-05-23 16:01:13 +0530

    Update an example to demo usage without llm

--- example/itinerary_planner.py
+++ example/itinerary_planner.py
@@ -1,43 +1,23 @@
-import os
-
-from openagi.actions.files import WriteFileAction
 from openagi.actions.tools.ddg_search import DuckDuckGoSearch
 from openagi.agent import Admin
-from openagi.llms.openai import OpenAIModel
-from openagi.planner.task_decomposer import TaskPlanner
 from rich.console import Console
 from rich.markdown import Markdown
 
 # Set OPENAI_API_KEY as environment variable
-os.environ["OPENAI_API_KEY"] = "sk-proj-xxxxxxxxxxxxxxxxxx"
+# os.environ["OPENAI_API_KEY"] = "sk-proj-xxxxxxxxxxxxxxxxxx"
 
-# Initialize the LLM
-config = OpenAIModel.load_from_env_config()
-llm = OpenAIModel(config=config)
-
-# Setup an Admin Agent
-admin = Admin(
-    llm=llm,
-    actions=[
-        DuckDuckGoSearch,
-        WriteFileAction,
-    ],  # Actions that the Agent can use to acheive the given objective
-    planner=TaskPlanner(
-        human_intervene=False,
-    ),
-)
-
-# Run the Agent with a query and description of the query.
-res = admin.run(
+# Setup and run Admin Agent
+response = Admin(actions=[DuckDuckGoSearch]).run(
     query="3 Days Trip to san francisco bay area",
     description="You are a knowledgeable local guide with extensive information about the city, it's attractions and customs",
 )
 
+
 # Print the results from the OpenAGI
 print("-" * 100)  # Separator
-Console().print(Markdown(res))
+Console().print(Markdown(response))
 
-# The Agent did some research using the given actions and share the itinerary.
+# The Agent did some research using the given actions and returned the below itinerary.
 """
 ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 ┃                                                                                                                Itinerary                                                                                                                 ┃
