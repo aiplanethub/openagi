9f45069060ca92bfb2eded88706fb785b3ce2557
ShreehariVaasishta <shreehari9481@gmail.com>
2024-05-24 12:15:13 +0530

    Make summarization optional

--- src/openagi/actions/tools/webloader.py
+++ src/openagi/actions/tools/webloader.py
@@ -1,38 +1,45 @@
-from typing import Any
-from openagi.actions.base import BaseAction
-from pydantic import Field
-
+import nltk
 from langchain_community.document_loaders import WebBaseLoader
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer
+from pydantic import Field
 from sumy.nlp.stemmers import Stemmer
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.summarizers.lsa import LsaSummarizer
 from sumy.utils import get_stop_words
 
-import nltk
-nltk.download('punkt')
+from openagi.actions.base import BaseAction
+from langchain.tools.playwright import NavigateTool, ExtractTextTool
+
+nltk.download("punkt")
+
 
 class WebBaseContextTool(BaseAction):
     """Use this Action to extract actual context from Web Search Tool"""
-    
+
     link: str = Field(
         default_factory=str,
-        description = "Extract context for the Agents from the Web Search",
+        description="Extract context for the Agents from the Web Search",
+    )
+    can_summarize: bool = Field(
+        default=True,
+        description="Indicates whether the action can summarize the content before returning. Defaults to True.",
     )
 
-    def _get_summary(self,data):    
+    def _get_summary(self, data):
         parser = PlaintextParser.from_string(data, Tokenizer("english"))
         summarizer = LsaSummarizer(Stemmer("english"))
         summarizer.stop_words = get_stop_words("english")
 
-        summary = summarizer(parser.document,6)
+        summary = summarizer(parser.document, 6)
         summary_sentences = [str(sentence) for sentence in summary]
         return " ".join(summary_sentences)
 
     def execute(self):
         loader = WebBaseLoader(self.link)
         data = loader.load()
-        metadata = data[0].metadata['title']
-        summarize_text = self._get_summary(data[0].page_content)
-        context = metadata+summarize_text
+        metadata = data[0].metadata["title"]
+        page_content = data[0].page_content
+        if self.can_summarize:
+            page_content = self._get_summary(page_content)
+        context = metadata + page_content
         return context
