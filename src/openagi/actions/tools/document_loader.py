94c5f1494604e72cc707d0ed76e5e62a05bfdb0a
Bhavyaaiplanet <bhavya@aiplanet.com>
2024-05-28 10:38:52 +0530

    added text and csv loader

---
+++ src/openagi/actions/tools/document_loader.py
@@ -0,0 +1,38 @@
+from openagi.actions.base import BaseAction
+from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from pydantic import Field
+
+
+class DocumentLoader(BaseAction):
+    """Use this Action to extract content from documents"""
+
+    file_path: str = Field(
+        default_factory=str,
+        description='Extract content from the File'
+    )
+
+    def text_loader(self):
+        loader = TextLoader.load(self.file_path)
+        data = loader.data()
+        page_content = data[0].page_content
+        meta_data = data[0].metadata['source']
+        context = meta_data + page_content
+        return context
+
+    def csv_loader(self):
+        content = ''
+        loader = CSVLoader(file_path=self.file_path)
+        data = loader.load()
+
+        for i in range(len(data)):
+            row_content = data[i].page_content
+            row_no = data[i].metadata['row']
+            content+= 'row_no' + ' ' + str(row_no) + ': ' + str(row_content)
+    
+    def execute(self):
+        if self.file_path.endswith('.txt'):
+            context = self.text_loader()
+        elif self.file_path.endswith('.csv'):
+            context = self.csv_loader()
+        return context
