ecb205a5ef8abb8701239c68e93820e7b9ba1c00
ShreehariVaasishta <shreehari9481@gmail.com>
2024-05-30 18:47:02 +0530

    Refactor and Improve task decomposer
    * Add retry logic on `JSONDecodeError`
    * Update promp retreival technique

--- src/openagi/planner/task_decomposer.py
+++ src/openagi/planner/task_decomposer.py
@@ -1,4 +1,7 @@
-from typing import Any, Dict, List, Optional, Union
+import json
+import logging
+import re
+from typing import Dict, List, Optional, Union
 
 from pydantic import Field
 
@@ -8,8 +11,9 @@ from openagi.exception import LLMResponseError
 from openagi.llms.azure import LLMBaseModel
 from openagi.planner.base import BasePlanner
 from openagi.prompts.base import BasePrompt
-from openagi.prompts.task_creator import SingleAgentTaskCreator, MultiAgentTaskCreator
-from openagi.utils.extraction import extract_ques_and_task, get_last_json
+from openagi.prompts.constants import CLARIFIYING_VARS
+from openagi.prompts.task_creator import MultiAgentTaskCreator, SingleAgentTaskCreator
+from openagi.utils.extraction import get_last_json
 from openagi.worker import Worker
 
 
@@ -21,29 +25,99 @@ class TaskPlanner(BasePlanner):
         default=HumanCLIInput,
         description="If `human_intervene` is enabled, which action to be performed.",
     )
-    prompt: Optional[BasePrompt] = Field(description="Prompt to be used")
+    prompt: Optional[BasePrompt] = Field(
+        description="Prompt to be used",
+        default_factory=str,
+    )
+    workers: Optional[List[Worker]] = Field(
+        default=None, description="List of workers to be used."
+    )
     llm: Optional[LLMBaseModel] = Field(default=None, description="LLM Model to be used")
+    retry_threshold: int = Field(
+        default=3, description="Number of times to retry the task if it fails."
+    )
 
     def get_prompt(self) -> None:
-        prompt = self.prompt
         if not self.prompt:
             if self.workers:
                 self.prompt = SingleAgentTaskCreator()
             else:
                 self.prompt = MultiAgentTaskCreator(workers=self.workers)
-        return prompt
+        logging.info(f"Using prompt: {self.prompt.__class__.__name__}")
+        return self.prompt
 
     def _extract_task_from_response(self, llm_response: str) -> Union[str, None]:
+        """
+        Extracts the last JSON object from the given LLM response string.
+
+        Args:
+            llm_response (str): The LLM response string to extract the JSON from.
+
+        Returns:
+            Union[str, None]: The last JSON object extracted from the response, or None if no JSON was found.
+        """
         return get_last_json(llm_response)
 
     def _should_clarify(self, query: Optional[str]) -> bool:
+        """
+        Determines whether the given query should be clarified.
+
+        Returns:
+            bool: True if the query should be clarified, False otherwise.
+        """
         if query and len(query) > 0:
             return True
         return False
 
+    def extract_ques_and_task(self, ques_prompt):
+        """
+        Extracts the question to be asked to the human and the remaining task from the given question prompt.
+
+        Args:
+            ques_prompt (str): The question prompt containing the question to be asked to the human and the remaining task.
+
+        Returns:
+            Tuple[str, str]: The task and the question to be asked to the human.
+        """
+        start = CLARIFIYING_VARS["start"]
+        end = CLARIFIYING_VARS["end"]
+        # pattern to find question to be asked to human
+        regex = rf"{start}(.*?){end}"
+
+        # Find all matches in the text
+        matches = re.findall(regex, ques_prompt)
+
+        # remove <clarify from human>...</clarify from human> part from the prompt
+        task = re.sub(regex, "", ques_prompt)
+        if not matches:
+            return None, None
+
+        question = matches[-1]
+        if question and question.strip():
+            f"OpenAGI: {question}\nYou: "
+        return task, question
+
     def plan(
-        self, query: str, description: str, supported_actions: List[Dict], *args, **kwargs
+        self,
+        query: str,
+        description: str,
+        supported_actions: List[Dict],
+        *args,
+        **kwargs,
     ) -> Dict:
+        """
+        Plans a task by querying a large language model (LLM) and extracting the resulting tasks.
+
+        Args:
+            query (str): The objective or query to plan for.
+            description (str): A description of the task or problem to solve.
+            supported_actions (List[Dict]): A list of dictionaries describing the actions that can be taken to solve the task.
+            *args: Additional arguments to pass to the LLM.
+            **kwargs: Additional keyword arguments to pass to the LLM.
+
+        Returns:
+            Dict: A dictionary containing the planned tasks.
+        """
         planner_vars = dict(
             objective=query,
             task_descriptions=description,
@@ -51,22 +125,51 @@ class TaskPlanner(BasePlanner):
             *args,
             **kwargs,
         )
-        prompt: str = self.prompt.from_template(
-            variables=planner_vars,
-        )
-        resp = self.llm.run(prompt)
+        prompt_template = self.get_prompt()
+        prompt: str = prompt_template.from_template(variables=planner_vars)
 
-        prompt, ques_to_human = extract_ques_and_task(resp)
+        print(f"\n\nOpenAGI: {prompt}\n\n")
+        resp = self.llm.run(prompt)
+        prompt, ques_to_human = self.extract_ques_and_task(resp)
 
         while self.human_intervene and self._should_clarify(ques_to_human):
             human_intervene = self.input_action(ques_prompt=ques_to_human)
             human_resp = human_intervene.execute()
             prompt = f"{prompt}\n{ques_to_human}\n{human_resp}"
             resp = self.llm.run(prompt)
-            prompt, ques_to_human = extract_ques_and_task(resp)
+            prompt, ques_to_human = self.extract_ques_and_task(resp)
 
-        tasks = self._extract_task_from_response(llm_response=resp)
+        tasks = self._extract_task_with_retry(resp, prompt)
+
         if not tasks:
             raise LLMResponseError("No tasks found in the Planner response.")
 
+        print(f"\n\nTasks: {tasks}\n\n")
         return tasks
+
+    def _extract_task_with_retry(self, llm_response: str, prompt: str) -> Dict:
+        """
+        Attempts to extract a task from the given LLM response, retrying up to a specified threshold if the response is not valid JSON.
+
+        Args:
+            llm_response (str): The response from the language model.
+            prompt (str): The prompt used to generate the LLM response.
+
+        Returns:
+            Dict: The extracted task, or raises an exception if the task could not be extracted after multiple retries.
+
+        Raises:
+            LLMResponseError: If the task could not be extracted after multiple retries.
+        """
+        retries = 0
+        while retries < self.retry_threshold:
+            try:
+                return self._extract_task_from_response(llm_response=llm_response)
+            except json.JSONDecodeError:
+                retries += 1
+                logging.info(
+                    f"Retrying task extraction {retries}/{self.retry_threshold} due to an error parsing the JSON response."
+                )
+                llm_response = self.llm.run(prompt)
+
+        raise LLMResponseError("Failed to extract tasks after multiple retries.")
