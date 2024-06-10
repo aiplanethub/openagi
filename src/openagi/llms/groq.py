ac4befd20721441f0801cdb848ecc250c056bf46
Tarun Jain <132284203+tarun-aiplanet@users.noreply.github.com>
2024-06-10 10:46:28 +0530

    Groq LLM support (#24)
    * Groq LLM support
    
    * fix huggingface hub
    
    ---------
    
    Co-authored-by: lucifertrj <tarun.19ec144@cambridge.edu.in>

---
+++ src/openagi/llms/groq.py
@@ -0,0 +1,63 @@
+from typing import Any
+from langchain_core.messages import HumanMessage
+from openagi.llms.base import LLMBaseModel, LLMConfigModel
+from openagi.utils.yamlParse import read_from_env
+
+try:
+   from langchain_groq import ChatGroq
+except ImportError:
+  raise OpenAGIException("Install langchain groq with cmd `pip install langchain-groq`")
+
+class GroqConfigModel(LLMConfigModel):
+    """Configuration model for Groq Chat model."""
+
+    groq_api_key: str
+    model_name: str = "mixtral-8x7b-32768"
+    temperature: float = 0.1
+
+class GroqModel(LLMBaseModel):
+    """Chat Groq Model implementation of the LLMBaseModel.
+
+    This class implements the specific logic required to work with Chat Groq Model.
+    """
+
+    config: Any
+
+    def load(self):
+        """Initializes the GroqModel instance with configurations."""
+        self.llm = ChatGroq(
+            model_name = self.config.model_name,
+            groq_api_key = self.config.groq_api_key,
+            temperature = self.config.temperature
+        )
+        return self.llm
+    
+    def run(self, input_data: str):
+        """Runs the Chat Groq model with the provided input text.
+
+        Args:
+            input_data: The input text to process.
+
+        Returns:
+            The response from Groq model with low inference latency.
+        """
+        if not self.llm:
+            self.load()
+        if not self.llm:
+            raise ValueError("`llm` attribute not set.")
+        message = HumanMessage(content=input_data)
+        resp = self.llm([message])
+        return resp.content
+    
+    @staticmethod
+    def load_from_env_config() -> GroqConfigModel:
+        """Loads the GroqModel configurations from a env file.
+
+        Returns:
+            An instance of GroqConfigModel with loaded configurations.
+        """
+        return GroqConfigModel(
+            groq_api_key=read_from_env("GROQ_API_KEY", raise_exception=True),
+            model_name = read_from_env("GROQ_MODEL",raise_exception=True),
+            temperature=read_from_env("GROQ_TEMP",raise_exception=True)
+        )
--- src/openagi/llms/hf.py
+++ src/openagi/llms/hf.py
@@ -1,13 +1,15 @@
-from transformers import pipeline
+from typing import Any
+from langchain_core.messages import HumanMessage
+from langchain_community.llms import HuggingFaceHub
 from openagi.llms.base import LLMBaseModel, LLMConfigModel
 from openagi.utils.yamlParse import read_from_env
 
-
 class HuggingFaceConfigModel(LLMConfigModel):
     """Configuration model for Hugging Face."""
-
-    huggingface_model: str
-
+    api_token: str
+    model_name: str = "huggingfaceh4/zephyr-7b-beta"
+    temperature: float = 0.1
+    max_new_tokens: int = 512
 
 class HuggingFaceModel(LLMBaseModel):
     """Hugging Face service implementation of the LLMBaseModel.
@@ -15,29 +17,35 @@ class HuggingFaceModel(LLMBaseModel):
     This class implements the specific logic required to work with Hugging Face service.
     """
 
-    def __init__(self, config: HuggingFaceConfigModel):
-        super().__init__(config)
+    config: Any
 
     def load(self):
-        """Initializes the Hugging Face model with configurations."""
-        self.llm = pipeline("text-generation", model=self.config.huggingface_model)
+        """Initializes the GroqModel instance with configurations."""
+        self.llm = HuggingFaceHub(
+            huggingfacehub_api_token = self.config.api_token,
+            repo_id= self.config.model_name, 
+            model_kwargs={"temperature": self.config.temperature,
+                          "max_new_tokens":self.config.max_new_tokens,
+                          "repetition_penalty":1.2}
+        )
         return self.llm
-
-    def run(self, input_text: str):
-        """Runs the Hugging Face model with the provided input text.
+    
+    def run(self, input_data: str):
+        """Runs the HuggingFace model with the provided input text.
 
         Args:
-            input_text: The input text to process.
+            input_data: The input text to process.
 
         Returns:
-            The response from Hugging Face service.
+            The response from HuggingFace model.
         """
         if not self.llm:
             self.load()
         if not self.llm:
             raise ValueError("`llm` attribute not set.")
-        resp = self.llm(input_text)
-        return resp[0]["generated_text"]
+        message = HumanMessage(content=input_data)
+        resp = self.llm([message])
+        return resp.content
 
     @staticmethod
     def load_from_env_config() -> HuggingFaceConfigModel:
@@ -47,5 +55,8 @@ class HuggingFaceModel(LLMBaseModel):
             An instance of HuggingFaceConfigModel with loaded configurations.
         """
         return HuggingFaceConfigModel(
-            huggingface_model=read_from_env("HUGGINGFACE_MODEL", raise_exception=True),
+            api_token = read_from_env("HUGGINGFACE_ACCESS_TOKEN",raise_exception=True),
+            model_name=read_from_env("HUGGINGFACE_MODEL", raise_exception=True),
+            temperature=read_from_env("TEMPERATURE",raise_exception=True),
+            max_new_tokens= read_from_env("MAX_NEW_TOKENS",raise_exception=True)
         )
