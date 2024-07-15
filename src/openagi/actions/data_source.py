b248ba384397873a19fdac5d7b75b4f5de16604f
Gourab Sinha <gourab@aiplanet.com>
2024-07-15 15:35:25 +0530

    Format __init__.py of tools and data_source.py

--- src/openagi/actions/data_source.py
+++ src/openagi/actions/data_source.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import Optional, Dict
 from openagi.actions.data_types import DataType
 
+
 class DataSource(BaseModel):
     type: DataType
     url: Optional[str] = None
